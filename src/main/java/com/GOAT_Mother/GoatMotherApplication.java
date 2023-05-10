6dc5bed3f37fe2ea5411f9400e158d136c3cb53f
amang33 <rjsgml4345@gmail.com>
2023-05-10 23:00:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/GOAT_Mother/GoatMotherApplication.java
@@ -0,0 +1,13 @@
+package com.GOAT_Mother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatMotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatMotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GOAT_Mother/GoatMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GOAT_Mother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatMotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
